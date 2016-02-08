4444c358ebdc064e40bc9e98cd41ab5879207807
levi koskan <levikoskan@gmail.com>
2016-02-08 15:02:18 -0600

    started making routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('hello');
+  Users.find({}, function(err, results){
+    if (err) throw err;
+    console.log(results);
+  });
   res.render('index', { title: 'TODO GAME' });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
