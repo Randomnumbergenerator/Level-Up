743ccf4aef7fe46f4f7af9d4092e6030ec9f679b
Joe Kantor <joekantor01@yahoo.com>
2016-02-11 08:17:52 -0600

    added secret for access

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(cookieParser());
+app.use(cookieParser("cat"));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // required for passport
